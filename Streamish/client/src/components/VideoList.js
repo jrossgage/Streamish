850b829ad4a42870f01f5e6a0f87310f86178b09
Joel Gage <jrossgage@gmail.com>
2021-07-12 11:54:44 -0500

    Video form and list are fully functional

--- Streamish/client/src/components/VideoList.js
+++ Streamish/client/src/components/VideoList.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from "react";
 import Video from './Video';
 import { getAllVideos, searchVideos } from "../modules/videoManager";
+import { VideoForm } from "./VideoForm";
 
 const VideoList = () => {
     const [videos, setVideos] = useState([]);
@@ -26,16 +27,21 @@ const VideoList = () => {
     }, [search]);
 
     return (
-        <div className="container">
-            <div >
-                <input type='text' className="search" required onChange={handleSearch} id="search_box" placeholder="Search" />
+        <>
+            <div className="container">
+                <div >
+                    <input type='text' className="search" required onChange={handleSearch} id="search_box" placeholder="Search" />
+                </div>
+
+                <VideoForm getVideos={getVideos} />
+
+                <div className="row justify-content-center">
+                    {videos.map((video) => (
+                        <Video video={video} key={video.id} />
+                    ))}
+                </div>
             </div>
-            <div className="row justify-content-center">
-                {videos.map((video) => (
-                    <Video video={video} key={video.id} />
-                ))}
-            </div>
-        </div>
+        </>
     );
 };
 
